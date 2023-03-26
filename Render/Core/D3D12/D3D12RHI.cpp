0d074ec2ff29355835c89f9f3a44012ea70679a8
Hannes Nisula <hannes@ducksquad.dev>
2023-03-26 21:28:32 +0200

    Fix an inactive usage of `DL_D3D12_CHECK` macro

--- Render/Core/D3D12/D3D12RHI.cpp
+++ Render/Core/D3D12/D3D12RHI.cpp
@@ -51,7 +51,7 @@ void D3D12RHI::InitFactory()
 
 	debugInterface->EnableDebugLayer();
 #else
-	DL_D3D12_THROW_FAIL(
+	DL_D3D12_CHECK(
 		CreateDXGIFactory2(IID_PPV_ARGS(&d3dFactory)),
 		"Failed to create DXGI factory");
 #endif
