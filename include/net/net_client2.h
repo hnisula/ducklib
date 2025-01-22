72a15e8737ce53ea506a71fb3b6c6705838a13aa
Hannes Nisula <hannes@ducksquad.dev>
2025-01-22 23:25:06 +0100

    WIP fixing `PacketHeader`

--- include/net/net_client2.h
+++ include/net/net_client2.h
@@ -5,48 +5,35 @@
 
 namespace ducklib::net
 {
-class NetClient2
-{
-public:
-    auto send_packet(Address to, std::span<std::byte> data) -> bool;
-    auto receive_packet() -> bool;
-
-private:
-    enum class PacketType
+    class NetClient2
     {
-        REGULAR = 0,
-        FRAGMENT = 1
+    public:
+        auto send_packet(Address to, std::span<std::byte> data) -> int;
+        auto receive_packet() -> bool;
+
+    private:
+        enum class PacketType
+        {
+            REGULAR = 0,
+            FRAGMENT = 1
+        };
+
+        struct PacketHeader
+        {
+            uint16_t sequence;
+            PacketType packet_type;
+
+            auto serialize(std::span<std::byte> packet_buffer) const -> std::span<std::byte>;
+            static auto deserialize(std::span<std::byte> packet_buffer) -> PacketHeader;
+        };
+
+        auto send_fragment_packets(Address to, std::span<std::byte> data) -> bool;
+
+        static constexpr int MAX_PACKET_SIZE = 1024;
+        static constexpr int MAX_PAYLOAD_SIZE = MAX_PACKET_SIZE; // TODO: Subtract header size
+
+        Socket socket;
     };
-
-    struct PacketHeader
-    {
-        uint16_t sequence;
-        PacketType packet_type;
-    };
-
-    auto send_fragment_packets(Address to, std::span<std::byte> data) -> bool;
-
-    static auto write_regular_packet_header(
-        std::span<std::byte> packet_buffer,
-        const PacketHeader* header) -> std::span<std::byte>;
-
-    static auto write_buffer_data(std::byte* buffer, const std::byte* data, int data_size) -> std::byte*;
-    template <typename T>
-    static auto write_buffer_data(std::byte* buffer, T) -> std::byte*;
-
-    static constexpr int MAX_PACKET_SIZE = 1024;
-    static constexpr int MAX_PAYLOAD_SIZE = MAX_PACKET_SIZE; // TODO: Subtract header size
-
-    Socket socket;
-};
-
-template <typename T>
-auto NetClient2::write_buffer_data(std::byte* buffer, T value) -> std::byte*
-{
-    static_assert(std::is_trivially_copyable_v<T>);
-
-    return write_buffer_data(buffer, reinterpret_cast<std::byte*>(&value), sizeof(T));
-}
 }
 
 #endif //NETCLIENT2_H
--- net/src/net_client2.cpp
+++ net/src/net_client2.cpp
@@ -2,38 +2,53 @@
 
 #include "net/net_client2.h"
 
-namespace ducklib::net
-{
-auto NetClient2::send_packet(Address to, std::span<std::byte> data) -> bool
-{
+#include <stdexcept>
+
+namespace ducklib::net {
+template <typename T>
+auto write_buffer_data(std::span<std::byte> buffer, T value) -> std::span<std::byte>;
+auto write_buffer_data(std::span<std::byte> buffer, const std::byte* data, int data_size) -> std::span<std::byte>;
+
+auto NetClient2::send_packet(Address to, std::span<std::byte> data) -> int {
     if (data.size() > MAX_PAYLOAD_SIZE)
-        return send_fragment_packets(to, data);
-    
-    write_regular_packet_header()
+        throw std::runtime_error("Data size exceeds maximum payload size");
+    // return send_fragment_packets(to, data);
 
-    return true;
+    auto packet_buffer = std::array<std::byte, MAX_PACKET_SIZE>{};
+    auto header = PacketHeader{ 0, PacketType::REGULAR };
+    std::span<std::byte> remaining_buffer(packet_buffer);
+
+    remaining_buffer = header.serialize(remaining_buffer);
+    remaining_buffer = write_buffer_data(remaining_buffer, data.data(), static_cast<int>(data.size()));
+
+    auto bytes_written = MAX_PACKET_SIZE - remaining_buffer.size();
+    auto bytes_sent = socket.send(to, std::span(packet_buffer.data(), bytes_written));
+
+    return bytes_sent;;
 }
 
-auto NetClient2::write_regular_packet_header(
-    std::span<std::byte> packet_buffer,
-    const PacketHeader* header) -> std::span<std::byte>
-{
-    auto sequence = htons(header->sequence);
-    auto packet_type = htons(static_cast<uint16_t>(header->packet_type));
-    auto* buffer_ptr = packet_buffer.data();
+auto NetClient2::PacketHeader::serialize(std::span<std::byte> packet_buffer) const -> std::span<std::byte> {
+    auto ordered_sequence = htons(sequence);
+    auto ordered_packet_type = htons(static_cast<uint16_t>(packet_type));
+    auto remaining_buffer = packet_buffer;
 
-    buffer_ptr = write_buffer_data(buffer_ptr, sequence);
-    buffer_ptr = write_buffer_data(buffer_ptr, packet_type);
+    remaining_buffer = write_buffer_data(remaining_buffer, ordered_sequence);
+    remaining_buffer = write_buffer_data(remaining_buffer, ordered_packet_type);
 
-    return packet_buffer.subspan(buffer_ptr - packet_buffer.data());
+    return remaining_buffer;
 }
 
-auto NetClient2::write_buffer_data(
-    std::byte* buffer,
-    const std::byte* data,
-    int data_size) -> std::byte*
-{
-    std::memcpy(buffer, data, data_size);
-    return buffer + data_size;
+template <typename T>
+auto write_buffer_data(std::span<std::byte> buffer, T value) -> std::span<std::byte> {
+    return write_buffer_data(buffer, reinterpret_cast<std::byte*>(&value), sizeof(value));
+}
+
+auto write_buffer_data(std::span<std::byte> buffer, const std::byte* data, int data_size) -> std::span<std::byte> {
+    if (data_size > buffer.size_bytes()) {
+        return std::span<std::byte>(); // TODO: Handle errors better
+    }
+
+    std::memcpy(buffer.data(), data, data_size);
+    return buffer.subspan(data_size);
 }
 }
