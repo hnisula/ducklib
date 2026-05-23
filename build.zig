d697c1884948729ade39dcd14e14ba67dfcc8e86
Hannes Nisula <hannes@ducksquad.dev>
2026-05-23 09:28:20 +0200

    Improve build setup a bit

--- build.zig
+++ build.zig
@@ -5,49 +5,32 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     // net
-    const net_module = b.createModule(.{
-        .root_source_file = b.path("src/net/net.zig"),
-        .target = target,
-        .optimize = optimize
-    });
+    const net_module = b.createModule(.{ .root_source_file = b.path("src/net/net.zig"), .target = target, .optimize = optimize });
     const net_lib = b.addLibrary(.{
         .name = "net",
         .linkage = .static,
         .root_module = net_module,
     });
+
+    // net-conn-test
+    const net_conn_test = b.addExecutable(.{ .name = "net-conn-test", .root_module = b.createModule(.{
+        .root_source_file = b.path("tests/net/conn_test.zig"),
+        .target = target,
+        .optimize = optimize,
+    }) });
+    net_conn_test.root_module.addImport("net", net_module);
+    const net_conn_test_install = b.addInstallArtifact(net_conn_test, .{});
+    const net_conn_test_step = b.step("net-conn-test", "Build net-conn-test program");
+    net_conn_test_step.dependOn(&net_conn_test_install.step);
+
+    // net tests
+    const net_tests = b.addTest(.{ .root_module = net_module });
+
+    // install build
     b.installArtifact(net_lib);
-    
-    // connection-test
-    const conn_test = b.addExecutable(.{
-        .name = "conn-test",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("tests/net/conn_test.zig"),
-            .target = target,
-            .optimize = optimize,
-        })
-    });
-    conn_test.root_module.addImport("net", net_module);
-    const conn_test_install = b.addInstallArtifact(conn_test, .{});
-    const conn_test_step = b.step("conn-test", "Compile connection test program");
-    conn_test_step.dependOn(&conn_test_install.step);
+    b.getInstallStep().dependOn(&net_conn_test_install.step);
 
-    // TEST tests
-    // const test_step = b.step("test", "Run unit tests");
-    // const unit_tests = b.addTest(.{ .root_module = b.createModule(.{
-    //     .root_source_file = b.path("tests/net/conn_test.zig"),
-    //     .target = target,
-    // }) });
-    // const socket_tests = b.addTest(.{ .root_module = b.createModule(.{
-    //     .root_source_file = b.path("src/net/Socket.zig"),
-    //     .target = target,
-    // }) });
-    // const run_unit_tests = b.addRunArtifact(unit_tests);
-    // const run_socket_tests = b.addRunArtifact(socket_tests);
-    // test_step.dependOn(&run_unit_tests.step);
-    // test_step.dependOn(&run_socket_tests.step);
-
-    // TEST debugging tests
-    // const test_artifact = b.addInstallArtifact(socket_tests, .{ .dest_dir = .{ .override = .{ .custom = "bin" } } });
-    // const install_test_step = b.step("install_test", "Create test binaries for debugging");
-    // install_test_step.dependOn(&test_artifact.step);
+    // test
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&b.addRunArtifact(net_tests).step);
 }
--- src/net/net.zig
+++ src/net/net.zig
@@ -1,3 +1,8 @@
+const std = @import("std");
 pub const Socket = @import("Socket.zig");
 pub const Connection = @import("Connection.zig");
 pub const Address = @import("Address.zig");
+
+test {
+    std.testing.refAllDecls(@This());
+}
